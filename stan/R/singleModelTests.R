ed6af618caf3154aed9883d9c95b76945ec46134
Diogo Melo <diogro@gmail.com>
2022-12-08 22:01:00 -0500

    pretty good bivariate model

--- stan/R/singleModelTests.R
+++ stan/R/singleModelTests.R
@@ -15,61 +15,6 @@ color_scheme_set("brightblue")
 
 source(here::here("simFunctions.R"))
 
-#### Fixed slopes ####
-
-N_id  = 250
-N_rep = 5
-N = N_id * N_rep
-
-beta_0 = 1
-h2_y = 0.4
-sigma_y = 1
-
-G = simulateKinship(N_id)
-
-x.add = rnorm(N_id)
-x = rep(x.add, each = N_rep) + rnorm(N)
-
-y.add = sigma_y * sqrt(h2_y) * t(chol(G)) %*% rnorm(N_id)
-y = rep(y.add, each = N_rep) + beta_0 * x + rnorm(N, sd = sigma_y * sqrt(1-h2_y))
-
-mod <- cmdstan_model(here::here("stan/models/univariate.stan"))
-# data {
-#   int<lower=0>    N;                     // number of observations
-#   int<lower=1> N_ids;                    // number of individuals
-#   array[N] int<lower=1, upper=N_ids> id; // individual IDs
-#   array[N] real Y;                       // y_2
-#   array[N] real X;                       // y_1
-#   cov_matrix[N_ids] A;                   // GRM matrix
-# }
-data_list = list(N = N, 
-                 N_ids = N_id,
-                 id = rep(1:N_id, each = N_rep),
-                 Y = y,
-                 X = x,
-                 A = as.matrix(G))
-
-fit <- mod$sample(
-  data = data_list,
-  seed = 123,
-  chains = 4,
-  parallel_chains = 4,
-  refresh = 500 # print update every 500 iters
-)
-fit$summary()
-png("test.png", height = 900, width = 1000)
-mcmc_recover_hist(fit$draws("mu_0"), 0) +
-mcmc_recover_hist(fit$draws("beta_0"), 1)
-dev.off()
-png("test.png", height = 900, width = 2000)
-mcmc_recover_intervals(fit$draws("a"), as.vector(y.add))
-dev.off()
-png("test.png", height = 900, width = 1000)
-data.frame(estimate = colMeans(colMeans(fit$draws("a"))), 
-           true = as.vector(y.add)) %>% ggplot(aes(true, estimate)) + geom_point() + geom_abline(intercept = 0, slope = 1) + theme_minimal()
-dev.off()
-
-
 #### Random slopes ####
 
 N_id  = 250
@@ -94,7 +39,7 @@ x = rep(x.add, each = N_rep) + rnorm(N)
 y.add = sigma_y * sqrt(h2_y) * t(chol(G)) %*% rnorm(N_id)
 y = rep(y.add, each = N_rep) + beta * x + rnorm(N, sd = sigma_y * sqrt(1-h2_y))
 
-mod <- cmdstan_model(here::here("stan/models/univariateRandomSlopes.stan"))
+mod_uni <- cmdstan_model(here::here("stan/models/univariateRandomSlopes.stan"))
 # data {
 #   int<lower=0>    N;                     // number of observations
 #   int<lower=1> N_ids;                    // number of individuals
@@ -103,35 +48,114 @@ mod <- cmdstan_model(here::here("stan/models/univariateRandomSlopes.stan"))
 #   array[N] real X;                       // y_1
 #   cov_matrix[N_ids] A;                   // GRM matrix
 # }
-data_list = list(N = N, 
-                 N_ids = N_id,
-                 id = rep(1:N_id, each = N_rep),
-                 Y = y,
-                 X = x,
-                 A = as.matrix(G))
-fit <- mod$sample(
-  data = data_list,
+data_list_uni = list(N = N, 
+                     N_ids = N_id,
+                     id = rep(1:N_id, each = N_rep),
+                     Y = y - mean(y),
+                     X = x,
+                     A = as.matrix(G))
+fit_uni <- mod_uni$sample(
+  data = data_list_uni,
   seed = 123,
   chains = 4,
   parallel_chains = 4,
   refresh = 500 # print update every 500 iters
 )
-fit$summary()
+fit_uni$summary()
 png("test.png", height = 900, width = 1000)
-(mcmc_recover_hist(fit$draws("mu_0"), 0) +
-mcmc_recover_hist(fit$draws("beta_0"), 1)) / 
-(mcmc_recover_hist(fit$draws("h2_y"), h2_y) +
-mcmc_recover_hist(fit$draws("h2_beta"), h2_beta))
+(mcmc_recover_hist(fit_uni$draws("mu_0"), 0) +
+mcmc_recover_hist(fit_uni$draws("beta_0"), 1)) / 
+(mcmc_recover_hist(fit_uni$draws("h2_y"), h2_y) +
+mcmc_recover_hist(fit_uni$draws("h2_beta"), h2_beta)) +
+  plot_layout(guides = 'collect')
 dev.off()
-png("test.png", height = 900, width = 2000)
-mcmc_recover_intervals(fit$draws("a"), as.vector(y.add))
-dev.off()
+
 png("test.png", height = 450, width = 1000)
-data.frame(estimate = colMeans(colMeans(fit$draws("a"))), 
-           true = as.vector(y.add)) %>% ggplot(aes(true, estimate)) + labs(x = "Simulated True Value", y = "Model Estimate") +
+data.frame(estimate = colMeans(colMeans(fit_uni$draws("a"))), 
+           true = as.vector(y.add)) |> ggplot(aes(true, estimate)) + labs(x = "Simulated True Value", y = "Model Estimate") +
            geom_point() + geom_abline(intercept = 0, slope = 1) + theme_minimal() + ggtitle("Breeding value BLUPs (g.add)") +
-data.frame(estimate = colMeans(colMeans(fit$draws("beta_add"))), 
-           true = as.vector(beta.add)) %>% ggplot(aes(true, estimate)) + labs(x = "Simulated True Value", y = "Model Estimate") +
+data.frame(estimate = colMeans(colMeans(fit_uni$draws("beta_add"))), 
+           true = as.vector(beta.add)) |> ggplot(aes(true, estimate)) + labs(x = "Simulated True Value", y = "Model Estimate") +
            geom_point() + geom_abline(intercept = 0, slope = 1) + theme_minimal() + ggtitle("Random Slope BLUPs (beta.add)")
 dev.off()
 
+#### Random slopes and genetic correlations ####
+
+N_id  = 250
+N_rep = 5
+N = N_id * N_rep
+
+h2_x = 0.4
+sigma_x = 1
+
+h2_y = 0.2
+sigma_y = 1
+
+h2_beta = 0.8
+sigma_beta = 1
+
+G = simulateKinship(N_id)
+
+rhog = 0.5
+varG = c(sigma_x * h2_x, sigma_y * h2_y)
+Sigma_xy = sqrt(varG) %*% t(sqrt(varG)) * matrix(c(1, 0.5, 0.5, 1), 2) 
+g_tilde = MASS::mvrnorm(N_id, c(0, 0), Sigma = diag(2))
+g.add = as.matrix(t(chol(G)) %*% g_tilde %*% chol(Sigma_xy))
+
+beta_0 = 1
+beta.add = sigma_beta * sqrt(h2_beta) * t(chol(G)) %*% rnorm(N_id)
+beta = beta_0 + rep(beta.add, each = N_rep) + rnorm(N, sd = sigma_beta * sqrt(1-h2_beta))
+
+x.add = g.add[,1]
+x = rep(x.add, each = N_rep) + rnorm(N, sd = sigma_x * sqrt(1-h2_x))
+
+y.add = g.add[,2]
+y = rep(y.add, each = N_rep) + rnorm(N, sd = sigma_y * sqrt(1-h2_y)) + beta * x
+
+mod_bi <- cmdstan_model(here::here("stan/models/bivariateRandomSlopes.stan"))
+# data {
+#   int<lower=0>    N;                     // number of observations
+#   int<lower=1> N_ids;                    // number of individuals
+#   array[N] int<lower=1, upper=N_ids> id; // individual IDs
+#   array[N] real Y;                       // y_2
+#   array[N] real X;                       // y_1
+#   cov_matrix[N_ids] A;                   // GRM matrix
+# }
+data_list_bi = list(N = N, 
+                    N_ids = N_id,
+                    id = rep(1:N_id, each = N_rep),
+                    Y = y - mean(y),
+                    X = x,
+                    A = as.matrix(G))
+fit_bi <- mod_bi$sample(
+  data = data_list_bi,
+  seed = 123,
+  chains = 8,
+  iter_warmup = 2000,
+  iter_sampling = 2000,
+  parallel_chains = 8,
+  adapt_delta = 0.99, max_treedepth = 13,
+  refresh = 500 # print update every 500 iters
+)
+fit_bi$summary()
+png("test.png", height = 900, width = 1000)
+(mcmc_recover_hist(fit_bi$draws("mu_x_0"), 0) +
+ mcmc_recover_hist(fit_bi$draws("mu_y_0"), 0) + 
+ mcmc_recover_hist(fit_bi$draws("beta_0"), 1)) / 
+(mcmc_recover_hist(fit_bi$draws("h2"), c(h2_x, h2_y)) +
+ mcmc_recover_hist(fit_bi$draws("h2_beta"), h2_beta) + 
+ mcmc_recover_hist(fit_bi$draws("rho"), rhog)) +
+  plot_layout(guides = 'collect')
+dev.off()
+
+{png("test.png", height = 450, width = 1000)
+data.frame(estimate = colMeans(colMeans(fit_bi$draws("a"))), 
+           true = c(as.vector(x.add), as.vector(y.add)), 
+           trait = rep(c("x", "y"), each = N_id)) |> 
+           ggplot(aes(true, estimate, color = trait)) + 
+           labs(x = "Simulated True Value", y = "Model Estimate") +
+           geom_point() + geom_abline(intercept = 0, slope = 1) + theme_minimal() + ggtitle("Breeding value BLUPs (g.add)") +
+data.frame(estimate = colMeans(colMeans(fit_bi$draws("beta_add"))), 
+           true = as.vector(beta.add)) |> ggplot(aes(true, estimate)) + labs(x = "Simulated True Value", y = "Model Estimate") +
+           geom_point() + geom_abline(intercept = 0, slope = 1) + theme_minimal() + ggtitle("Random Slope BLUPs (beta.add)")
+dev.off()}
--- stan/models/bivariateRandomSlopes.stan
+++ stan/models/bivariateRandomSlopes.stan
@@ -8,42 +8,79 @@ data {
 }
 transformed data{
   matrix[N_ids, N_ids] LA;
+  int K = 2;                             // number of traits
   LA = cholesky_decompose(A);
 }
 parameters {
   real beta_0;                       // slope mean
-  real mu_0;                         // y_2 trait mean
-  real<lower=0> sigma_y;             // y_2 std
-  real<lower=0> sigma_beta;          // beta std
-  real<lower=0, upper = 1> h2_y;     // variance partition of additive effects
+  real mu_x_0;                       // y_1 trait mean
+  real mu_y_0;                       // y_2 trait mean
+
+  real<lower=0> sigma_beta;          // beta sd
   real<lower=0, upper = 1> h2_beta;  // variance partition of additive effects
-  vector[N_ids]  a_tilde;            // breeding values
-  vector[N_ids]  beta_tilde;         // random slopes
-  vector[N]      beta;               
+  vector[N_ids]  beta_tilde;         // random slopes precursor
+  vector[N]      beta;     
+
+ 
+  vector<lower=0>[K] L_sigma;        // total trait sd
+  vector<lower=0, upper = 1>[K] h2;  // variance partition of both traits
+  matrix[N_ids, K] a_tilde;          // breeding values precursor
+  cholesky_factor_corr[K] L_Omega_G; // G matrix         
 }
 transformed parameters {
-    vector[N_ids] a;
-    vector[N_ids] beta_add;
-    a = sigma_y * sqrt(h2_y) * (LA * a_tilde);
-    beta_add = sigma_beta * sqrt(h2_beta) * (LA * beta_tilde);
+  vector[N_ids] beta_add;        // random slopes
+  matrix[N_ids, K] a;            // breeding values
+  vector<lower=0>[K] L_sigma_G;  // trait genetic sd
+  vector<lower=0>[K] L_sigma_R;  // trait residual sd
+
+  for(k in 1:K){
+    L_sigma_G[k] = L_sigma[k] * sqrt(h2[k]);
+    L_sigma_R[k] = L_sigma[k] * sqrt(1-h2[k]);
+  }
+  a = (LA * a_tilde) * diag_pre_multiply(L_sigma_G, L_Omega_G)'; // a ~ N(0, A x G)
+  beta_add = sigma_beta * sqrt(h2_beta) * (LA * beta_tilde);
 }
 model {
-    vector[N] mu;
+    vector[N] mu_x;
+    vector[N] mu_y;
     vector[N] mu_beta;
     
+    // per observation mean parameters
     for(n in 1:N){
       mu_beta[n] = beta_0 + beta_add[id[n]];
-      mu[n] = mu_0 + beta[n] * X[n] + a[id[n]];
+      mu_x[n] = mu_x_0 + a[id[n], 1];
+      mu_y[n] = mu_y_0 + a[id[n], 2] + beta[n] * X[n];
     }
-    beta ~ normal(mu_beta, sigma_beta*sqrt(1 - h2_beta));
-    Y ~ normal(mu, sigma_y*sqrt(1 - h2_y));
 
+    // residual variation likelihoods and priors
+    beta ~ normal(mu_beta, sigma_beta*sqrt(1 - h2_beta));
+    X ~ normal(mu_x, L_sigma_R[1]);
+    Y ~ normal(mu_y, L_sigma_R[2]);
+
+    // Intercept priors
     beta_0 ~ normal(0, 1);
-    mu_0 ~ normal(0, 1);
-    sigma_y ~ normal(0, 1);
-    h2_y ~ beta(2, 4);
+    mu_x_0 ~ normal(0, 1);
+    mu_y_0 ~ normal(0, 1);
+
+    // sd priors
     sigma_beta ~ normal(0, 1);
+    L_sigma ~ normal(0, 1);
+
+    // variance partition priors
     h2_beta ~ beta(2, 4);
-    a_tilde ~ normal(0, 1);
+    h2 ~ beta(2, 2);
+
+    // random effect precursors
     beta_tilde ~ normal(0, 1);
+    to_vector(a_tilde) ~ normal(0, 1);
+    
+    // trait genetic correlation prior
+    L_Omega_G ~ lkj_corr_cholesky(4);
+}
+generated quantities {
+    corr_matrix[K] corrG;
+    real rho;
+
+    corrG = multiply_lower_tri_self_transpose(L_Omega_G);
+    rho = corrG[1,2];
 }
